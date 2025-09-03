c6ef9598770659cc0324b72b11519c9002ebc252
cde123456781 <bjc199@uclive.ac.nz>
2025-09-04 02:45:26 +1200

    Adds MonoGameLibrary and Logo

--- Dungeon Slime/Game1.cs
+++ Dungeon Slime/Game1.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
 
 namespace Dungeon_Slime
 {
-    public class Game1 : Game
+    public class Game1 : Core
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _logo;
 
-        public Game1()
+        public Game1(): base("Dungeon Slime", 1280, 720, false)
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            
         }
 
         protected override void Initialize()
@@ -25,8 +25,7 @@ namespace Dungeon_Slime
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            _logo = Content.Load<Texture2D>("images/logo");
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,7 +41,70 @@ namespace Dungeon_Slime
 
         protected override void Draw(GameTime gameTime)
         {
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
+            Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
+            SpriteBatch.Begin(SpriteSortMode.FrontToBack);
+            SpriteBatch.Draw(
+                _logo,
+                new Vector2
+                (
+                    (Window.ClientBounds.Width) * 0.5f,
+                    (Window.ClientBounds.Height) * 0.5f
+       
+                ),
+                iconSourceRect,   // source rectangle
+                Color.Blue * 1f,    // Colour
+                MathHelper.ToRadians(0),   // Rotation
+                new Vector2
+                (
+                    iconSourceRect.Width / 2,
+                    iconSourceRect.Height /2
+                    
+                ),   // Origin
+                new Vector2
+                (
+                   1.5f,
+                   1f
+                ),   // Scale
+                SpriteEffects.None,   // effects
+                1.0f    // layer depth
+            );
+
+
+            SpriteBatch.Draw(
+                _logo,
+                new Vector2
+                (
+                    (Window.ClientBounds.Width) * 0.5f,
+                    (Window.ClientBounds.Height) * 0.5f
+
+                ),
+                wordmarkSourceRect,   // source rectangle
+                Color.Blue * 0.5f,    // Colour
+                MathHelper.ToRadians(0),   // Rotation
+                new Vector2
+                (
+                    wordmarkSourceRect.Width / 2,
+                    wordmarkSourceRect.Height / 2
+
+                ),   // Origin
+                new Vector2
+                (
+                   1.5f,
+                   1f
+                ),   // Scale
+                SpriteEffects.None,   // effects
+                0.0f    // layer depth
+            );
+
+
+
+            SpriteBatch.End();
+            
 
             // TODO: Add your drawing code here
 
---
+++ MonoGameLibrary/Core.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameLibrary;
+
+
+
+public class Core: Game 
+{
+    internal static Core s_instance;
+
+    /// <summary>
+    /// Gets a reference to the Core instance
+    /// </summary>
+    public static Core Instance => s_instance;
+
+    /// <summary>
+    /// Gets the graphics device manager to control the presentation of graphics
+    /// </summary>
+    public static GraphicsDeviceManager Graphics { get; private set; }
+
+    /// <summary>
+    /// Gets the graphics device used to create graphical resources and perform primitive rendering
+    /// </summary>
+    public static new GraphicsDevice GraphicsDevice { get; private set; }
+
+    /// <summary>
+    /// Gets the sprite batch used for all 2D rendering
+    /// </summary>
+    public static SpriteBatch SpriteBatch { get; private set; }
+
+    /// <summary>
+    /// Gets the content manager used to load global assets
+    /// </summary>
+    public static new ContentManager Content { get; private set; }
+
+    /// <summary>
+    /// Creates a new Core instance
+    /// </summary>
+    /// <param name="title">The title to display in the title bar of the game</param>
+    /// <param name="width">The width of the window</param>
+    /// <param name="height">The height of the window</param>
+    /// <param name="fullScreen">Indicates if the window should start in fullscreen</param>
+    public Core(string title, int width, int height, bool fullScreen)
+    {
+        if (s_instance != null)
+        {
+            throw new InvalidOperationException($"Only a single Core instance can be created");
+        }
+
+        s_instance = this;
+
+        Graphics = new GraphicsDeviceManager(this);
+
+        Graphics.PreferredBackBufferHeight = height;
+        Graphics.PreferredBackBufferWidth = width;
+        Graphics.IsFullScreen = fullScreen;
+
+        Graphics.ApplyChanges();
+
+        Window.Title = title;
+
+        Content = base.Content;
+
+        Content.RootDirectory = "Content";
+
+        IsMouseVisible = true;
+
+    }
+
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        GraphicsDevice = base.GraphicsDevice;
+
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+    }
+
+}
