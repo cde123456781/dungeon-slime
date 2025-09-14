2ec694ce669b93e56b6c5a9a3ac323ea5d345c7c
cde123456781 <bjc199@uclive.ac.nz>
2025-09-14 15:42:40 +1200

    Add scrolling background for title scene

--- Dungeon Slime/Scenes/TitleScene.cs
+++ Dungeon Slime/Scenes/TitleScene.cs
@@ -25,6 +25,12 @@ public class TitleScene : Scene
     private Vector2 _pressEnterOrigin;
 
 
+    private Texture2D _backgroundPattern;
+    private Rectangle _backgroundDestination;
+    private Vector2 _backgroundOffset;
+    private float _scrollSpeed = 50.0f;
+
+
     public override void Initialize()
     {
         base.Initialize();
@@ -41,6 +47,10 @@ public class TitleScene : Scene
         size = _font.MeasureString(PRESS_ENTER_TEXT);
         _pressEnterPos = new Vector2(640, 620);
         _pressEnterOrigin = size * 0.5f;
+
+        _backgroundOffset = Vector2.Zero;
+        _backgroundDestination = Core.GraphicsDevice.PresentationParameters.Bounds;
+
     }
 
 
@@ -48,6 +58,7 @@ public class TitleScene : Scene
     {
         _font = Core.Content.Load<SpriteFont>("fonts/04B_30");
         _font5x = Content.Load<SpriteFont>("fonts/04B_30_5x");
+        _backgroundPattern = Content.Load<Texture2D>("images/background-pattern");
     }
 
     public override void Update(GameTime gameTime)
@@ -56,11 +67,24 @@ public class TitleScene : Scene
         {
             Core.ChangeScene(new GameScene());
         }
+
+
+        float offset = _scrollSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _backgroundOffset.X -= offset;
+        _backgroundOffset.Y -= offset;
+
+        _backgroundOffset.X %= _backgroundPattern.Width;
+        _backgroundOffset.Y %= _backgroundPattern.Height;
     }
 
     public override void Draw(GameTime gameTime)
     {
         Core.GraphicsDevice.Clear(new Color(32, 40, 78, 255));
+
+        Core.SpriteBatch.Begin(samplerState: SamplerState.PointWrap);
+        Core.SpriteBatch.Draw(_backgroundPattern, _backgroundDestination, new Rectangle(_backgroundOffset.ToPoint(), _backgroundDestination.Size), Color.White * 0.5f);
+        Core.SpriteBatch.End();
+
 
         Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
